ffcc8778fcd394f0978df2d22e61b9ea7e78c319
Ashish Kumar <aashishrana107@gmail.com>
2021-03-12 03:33:50 +0530

    Authorize Administration Role

--- leave-management/Startup.cs
+++ leave-management/Startup.cs
@@ -1,3 +1,4 @@
+using leave_management.Areas;
 using leave_management.Contracts;
 using leave_management.Data;
 using leave_management.Mappings;
@@ -42,13 +43,15 @@ namespace leave_management
             services.AddAutoMapper(typeof(Maps));
 
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env,UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
         {
             if (env.IsDevelopment())
             {
@@ -68,6 +71,8 @@ namespace leave_management
 
             app.UseAuthentication();
             app.UseAuthorization();
+
+            SeedData.Seed(userManager,roleManager);
 
             app.UseEndpoints(endpoints =>
             {
